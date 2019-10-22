32b26144d8d63655ec253969237e8e3e6dec8721
jion-kozono <zeon66190329@gmail.com>
2019-10-22 14:47:14 +0900

    課題：配列を宣言して出力しましょう

---
+++ Assets/Test1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        int[] array = { 20, 30, 40, 50, 60 };
+        for (int i = 0; i < array.Length; i++)
+        {
+            //順番通りに表示する
+            Debug.Log(array[i]);
+
+            //逆順に表示する
+            Debug.Log(array[array.Length - i - 1]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
